926c2af8792837851cad44af313cadc2b1f9dc94
Troy Geiger <troygeiger@users.noreply.github.com>
2021-11-17 22:15:35 -0500

    Initial Commit

---
+++ sieve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
